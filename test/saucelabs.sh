9d02bfaac605b56f55a429786efcfbd4ad7661da
John Howes <john.howes@gmail.com>
2017-03-21 10:48:20 -0500

    Experimenting with Saucelabs

--- test/saucelabs.sh
+++ test/saucelabs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 { read url; echo "Listening at $url";  }
 
-curl https://saucelabs.com/rest/v1/johnhowes/js-tests -X POST -H 'Content-Type: application/json' --data '{  "platforms": [["Windows 7", "firefox", "27"],  ["Linux", "googlechrome", ""]],  "url": "$url",  "framework": "mocha"}'
+curl https://saucelabs.com/rest/v1/$SAUCE_USERNAME/js-tests -X POST -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -H 'Content-Type: application/json' --data '{  "platforms": [["Windows 7", "firefox", "27"],  ["Linux", "googlechrome", ""]],  "url": "$url",  "framework": "mocha"}'
