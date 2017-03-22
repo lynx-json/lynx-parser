06c53d27e06890878651109e5e86ff8d80161a46
John Howes <john.howes@gmail.com>
2017-03-22 14:14:36 -0500

    Attempting Additional Platforms

--- test/saucelabs.sh
+++ test/saucelabs.sh
@@ -9,7 +9,14 @@ then
   exit 0
 fi
 
-options='{  "platforms": [["Windows 7", "firefox", "27"],  ["Linux", "googlechrome", ""]],  "url": "'$url'", "tunnelIdentifier": "'$TRAVIS_JOB_NUMBER'",  "framework": "mocha"}';
+options='{  "platforms": [
+  ["Windows 10", "firefox", "51"],
+  ["Windows 7", "firefox", "51"],
+  ["Windows 7", "ie", "11"],
+  ["Windows 7", "ie", "10"],
+  ["Linux", "googlechrome", ""]
+  ],  "url": "'$url'", 
+  "tunnelIdentifier": "'$TRAVIS_JOB_NUMBER'",  "framework": "mocha"}';
 
 tests=$(curl https://saucelabs.com/rest/v1/$SAUCE_USERNAME/js-tests -X POST -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -H 'Content-Type: application/json' -d "$options")
 
