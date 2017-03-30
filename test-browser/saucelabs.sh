5fa26beb19334c26896fd7ee917cd71ee168c8ce
John Howes <john.howes@gmail.com>
2017-03-30 13:13:40 -0500

    Reorg of Browser Testing Prior to Replicating

---
+++ test-browser/saucelabs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+read url; 
+echo "Listening at $url";
+
+if [ -z "$TRAVIS_JOB_NUMBER" ]
+then
+  open $url
+else
+  options='{  "platforms": [
+    ["Windows 10", "firefox", "51"],
+    ["Windows 7", "firefox", "51"],
+    ["Windows 7", "internet explorer", "11"],
+    ["Windows 7", "internet explorer", "10"],
+    ["Linux", "googlechrome", ""]
+    ],  "url": "'$url'", 
+    "tunnelIdentifier": "'$TRAVIS_JOB_NUMBER'",  "framework": "mocha"}';
+
+  tests=$(curl https://saucelabs.com/rest/v1/$SAUCE_USERNAME/js-tests -X POST -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -H 'Content-Type: application/json' -d "$options")
+
+  completed=false
+  status=unknown
+  while [ "$completed" != 'true' ]
+  do
+    result=$(curl https://saucelabs.com/rest/v1/$SAUCE_USERNAME/js-tests/status -X POST -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -H 'Content-Type: application/json' -d "$tests")
+    completed=$(echo "$result" | jq -r '.completed')
+    
+    if [ "$completed" != 'true' ]
+    then
+      echo "$result" | jq
+      echo "Waiting for results"
+      sleep 5
+    else
+      failures=$(echo "$result" | jq -r '."js tests"[].result.failures' | grep '[1-9]')
+      
+      echo "$result" | jq -r '."js tests"[]'
+
+      if [ -z "$failures" ]
+      then
+        status="pass"
+      else
+        status="fail"
+      fi
+    fi
+  done
+
+  curl $url'/stop'
+
+  if [ "$status" == 'pass' ]
+  then
+    exit 0
+  else
+    exit 1
+  fi
+fi
